cde81edadcf5f17b498b5fe8cfe787ad94a0456d
Kultur i Halland - Regionbibliotek <viktor.sarge@regionhalland.se>
2013-05-10 22:10:41 +0300

    Rensade överbliven kod.

--- samsok.py
+++ samsok.py
@@ -122,35 +122,11 @@ class connectorclass:
         content = page.read()
         page.close()     
         return content 
- 
-from HTMLParser import HTMLParser
-import htmlentitydefs
-
-class HTMLTextExtractor(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.result = [ ]
-
-    def handle_data(self, d):
-        self.result.append(d)
-
-    def handle_charref(self, number):
-        codepoint = int(number[1:], 16) if number[0] in (u'x', u'X') else int(number)
-        self.result.append(unichr(codepoint))
-
-    def handle_entityref(self, name):
-        codepoint = htmlentitydefs.name2codepoint[name]
-        self.result.append(unichr(codepoint))
-
-    def get_text(self):
-        return u''.join(self.result)
- 
+  
 def strip_tags(raw_html):
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr,'', raw_html)
     return cleantext
- 
- 
         
 class opacParser:
     'Knows how to parse the different library opacs'
